483100df26c7d5f56eb1a46af7726b50d5223c59
IsabelaSouto <belasoutomaior@gmail.com>
2023-11-09 19:45:10 -0300

    requisito 5 finalizado

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,5 +1,47 @@
+import { useSelector } from 'react-redux';
+import { GlobalSatetType } from '../services/types';
+
 function Table() {
-  return <div>Table</div>;
+  const { expenses } = useSelector((state: GlobalSatetType) => state.wallet);
+
+  const expensesTable = expenses.map((expense) => {
+    const convertedValues = (Number(expense.value)
+    * Number(expense.exchangeRates[expense.currency].ask)).toFixed(2);
+
+    return (
+      <tr key={ expense.id }>
+        <td>{ expense.description }</td>
+        <td>{ expense.tag }</td>
+        <td>{ expense.method }</td>
+        <td>{ Number(expense.value).toFixed(2) }</td>
+        <td>{ expense.exchangeRates[expense.currency].name }</td>
+        <td>{ Number(expense.exchangeRates[expense.currency].ask).toFixed(2) }</td>
+        <td>{ convertedValues }</td>
+        <td>Real</td>
+        <button>Editar</button>
+        <button>Excluir</button>
+      </tr>
+    );
+  });
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <th>Descrição</th>
+          <th>Tag</th>
+          <th>Método de pagamento</th>
+          <th>Valor</th>
+          <th>Moeda</th>
+          <th>Câmbio utilizado</th>
+          <th>Valor convertido</th>
+          <th>Moeda de conversão</th>
+          <th>Editar/Excluir</th>
+        </thead>
+        <tbody>{ expensesTable }</tbody>
+      </table>
+    </div>
+  );
 }
 
 export default Table;
